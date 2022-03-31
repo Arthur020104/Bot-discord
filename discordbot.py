0efdedf9cc1bde6f53883ba1a01908c68e0bd387
Arthur <arthur020104@gmail.com>
2022-03-31 18:05:26 -0300

    finishing paulada and adding the random method to talk to fri

--- discordbot.py
+++ discordbot.py
@@ -5,23 +5,13 @@ from cs50 import SQL
 
 
 db = SQL("sqlite:///database.db")
-"""
-PAULADAS = {
-    "Arthur": 0,
-    "Lucas": 0,
-    "Kaua":0,
-    "Johnny":0,
-    "Nicolas":0,
-    "Asuna": 0,
-    "Sucata":0,
-}
-"""
+
 def name(string):
     name = ''
     for i in range(9,(len(string)),1):
         name += string[i]
 
-    return name
+    return name.lower()
 
 paulada = ["O menino tá quente.", "Nossa muito forte.","Simplesmente o melhor."]
 class MyClient(discord.Client):
@@ -33,26 +23,27 @@ class MyClient(discord.Client):
             await message.channel.send("Fala meu nome lixo.")
         
         if message.author.name == 'Arthur;..':
-            if random.randrange(0, 3) == 1:
+            if random.randrange(0, 10) == 1:
                 await message.channel.send("Lixo.")
 
         if message.author.name == 'Ambrósio':
-            if random.randrange(0, 3) == 1:
+            if random.randrange(0, 10) == 1:
                 await message.channel.send("Lucas cala a boca lixo.")
 
         if message.author.name == 'Kaua Garcia':
-            await message.channel.send("Porra Kaua ninguem te aguenta mais.")
+            if random.randrange(0, 10) == 1:
+                await message.channel.send("Porra Kaua ninguem te aguenta mais.")
         
         if "!paulada" in message.content:
             PAULADAS = db.execute("SELECT * FROM PAULADA WHERE user = ? ", name(message.content))
-            print(PAULADAS)
-            if PAULADAS[0]:
+            if PAULADAS != []:
                 count = PAULADAS[0]['paulada_count']
                 count += 1
-                await message.channel.send(f'{paulada[random.randrange(0, len(paulada))]}{os.linesep}O {name(message.content)} vem acumulando {count} pauladas.')
+                await message.channel.send(f'{paulada[random.randrange(0, len(paulada))]}{os.linesep}{(name(message.content)).capitalize()} vem acumulando {count} pauladas.')
                 db.execute("UPDATE PAULADA SET paulada_count = ? WHERE id = ? AND user = ?",count,PAULADAS[0]['id'],PAULADAS[0]['user'])
             else:
                 db.execute("INSERT INTO PAULADA(paulada_count,user) VALUES( ?, ?);",1,name(message.content))
+                await message.channel.send(f'{paulada[random.randrange(0, len(paulada))]}{os.linesep}{(name(message.content)).capitalize()} vem acumulando {1} pauladas.')
 client = MyClient()
 client.run('OTU5MDUwMDY3MTA3NTMyODAw.YkWO-g.20AJ1d1o3tqBgg6-2NQHJRVAKvc')
 
